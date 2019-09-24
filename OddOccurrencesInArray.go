ea961f3726a9c166d5537fa04badee66e33dcf42
Mandeep Khatry <072bct519@ioe.edu.np>
2019-09-24 14:40:37 +0545

    Lesson 2 : OddOccurrencesInArray

--- OddOccurrencesInArray.go
+++ OddOccurrencesInArray.go
@@ -2,7 +2,7 @@ package main
 
 //https://app.codility.com/programmers/lessons/2-arrays/odd_occurrences_in_array/
 
-// you can also use imports, for example:
+// you can also use imports, for example
 import "fmt"
 
 // you can write to stdout for debugging purposes, e.g.
